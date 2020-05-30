9679a5445bc5e04fdf67c1ad36899c46bda573c9
ilya-dm <ilyadmitren@mail.ru>
2020-05-30 20:43:07 +0700

    Добавлены дополнительные параметры, поправлены тесты

--- app.py
+++ app.py
@@ -2,53 +2,47 @@ from pprint import pprint
 import requests
 import time
 import json
-from data import app_id, vk_api
 from datetime import datetime
 import re
-from pandas import json_normalize
 from pymongo import MongoClient
 
+
 class User:
-    def __init__(self, id):
+    def __init__(self, id, access_token=''):
         self.id = id
-        self.access_token = self.authorization()
         self.delta = 4
         self.V = '5.103'
         self.API = "https://api.vk.com/method"
+        self.app_id = 7451978
+        self.access_token = access_token
+
     def authorization(self):
-        url = f'https://oauth.vk.com/authorize?client_id={app_id}&display=popup&redirect_uri=https://oauth.vk.com' \
+        url = f'https://oauth.vk.com/authorize?client_id={self.app_id}&display=popup&redirect_uri=https://oauth.vk.com' \
               f'/blank.html&scope=friends&response_type=token&v=5.103 '
         print(f'Чтобы получить токен для авторизации, перейдите по ссылке: {url}')
-
-        access_token = input("Введите токен: ")
-        return access_token
+        self.access_token = input("Введите токен: ")
+        return self.access_token
 
     def get_groups(self):
-        group_list = list()
         group_set = set()
         params = {'access_token': self.access_token, 'user_id': self.resolveScreenName(self.id), 'v': self.V,
-                  'extended': 1, 'fields': ['members_count']}
-        response = requests.get(f'{self.API}/groups.get', params=params).json()['response']
-        if response is not None:
+                  'extended': 0}
+        try:
+            response = requests.get(f'{self.API}/groups.get', params=params).json()['response']
             items = response.get('items')
-            for item in items:
-                try:
-                    description = {'gid': item['id'], 'name': item['name'], 'members_count': item['members_count']}
-                    group_list.append(description)
-                    group_set.add(item['id'])
-                except KeyError:
-                    continue
+            group_set = set(items)
+        except KeyError:
+            pass
         return group_set
 
     def resolveScreenName(self, screen_name):
         params = {'access_token': self.access_token, 'screen_name': screen_name, 'v': self.V}
-        response = requests.get(f"{self.API}/utils.resolveScreenName", params=params).json()['response']
-        ID = response['object_id']
+        ID = requests.get(f"{self.API}/utils.resolveScreenName", params=params).json()['response']['object_id']
         return ID
 
     def getUserInfo(self):
         params = {'access_token': self.access_token, 'user_ids': self.resolveScreenName(self.id), 'fields':
-            'city ,sex, bdate, music', 'v': self.V}
+            'city ,sex, bdate, music, movies', 'v': self.V}
         response = requests.get(f"{self.API}/users.get", params=params).json()['response'][0]
         for data in response:
             if response[data] == '':
@@ -57,19 +51,19 @@ class User:
         if 'city' not in response:
             city = input("Введите город: ")
             params = {'access_token': self.access_token, 'country_id': 1, 'q': city, 'v': self.V}
-            city_request = requests.get(f"{self.API}/database.getCities", params=params).json()['response']
-            city_request = city_request['items'][0]
+            city_request = requests.get(f"{self.API}/database.getCities", params=params).json()['response']['items'][0]
             response['city'] = city_request
         if 'bdate' not in response:
             bdate = input('Введите дату рождения в формате дд.мм.гггг: ')
             response['bdate'] = bdate
         if len(response['bdate']) < 8:
             response['bdate'] += '.' + input('Введите год рождения:')
+        if 'music' not in response:
+            response['music'] = input("Введите любимую музыку: ")
         return response
 
     def search_users_by_filter(self):
         target = self.getUserInfo()
-        common_music = list()
         db_list = list()
         if target['sex'] == 2:
             filter_sex = 1
@@ -77,71 +71,97 @@ class User:
             filter_sex = 2
         filter_city = target['city']['id']
         today = datetime.strptime(datetime.today().strftime("%d.%m.%Y"), "%d.%m.%Y")
-        bdate = datetime.strptime(target['bdate'], "%d.%m.%Y")
-
+        bday = datetime.strptime(target['bdate'], "%d.%m.%Y")
+        age = today.year - bday.year
         db_collection = self.connect_to_db()
         db_users = list(db_collection.find({}, {'id': 1}))
         for index in db_users:
             db_list.append(index['id'])
-        filter_age_min = int((today - bdate).days / 365) - self.delta
-        filter_age_max = int((today - bdate).days / 365) + self.delta
         params = {'access_token': self.access_token, 'count': 1000, 'offset': len(db_users), 'city': filter_city,
-                  'sex': filter_sex, 'status': 6, 'age_from': filter_age_min,
-                  'age_to': filter_age_max, 'has_photo': 1, 'fields': 'music', 'v': self.V}
+                  'sex': filter_sex, 'status': 6, 'age_from': age-self.delta,
+                  'age_to': age+self.delta, 'has_photo': 1, 'fields': 'music', 'v': self.V}
         searched_users = requests.get(f"{self.API}/users.search", params=params).json()['response']['items']
-        for index in range(len(searched_users)-1, -1, -1):
+        for index in range(len(searched_users) - 1, -1, -1):
             searched_users[index]['weight'] = 0
             if searched_users[index]['id'] in db_list:
                 searched_users.remove(searched_users[index])
-        try:
-            music_filter = target['music']
-            if ',' in music_filter:
-                music_filter = music_filter.split(',')
-            for user in searched_users:
-                if type(music_filter) == list:
-                    try:
-                        for element in music_filter:
-                            a = re.search(re.compile(element, re.IGNORECASE), user['music'])
-                            common_music.append(a)
-                        if len(common_music) > 0:
-                            user['weight'] += 3
-                            common_music = []
-                    except KeyError:
-                        continue
-                else:
-                    try:
-                        a = re.search(re.compile(music_filter, re.IGNORECASE), user['music'])
-                        common_music.append(a)
-                        if len(common_music) > 0:
-                            user['weight'] += 3
-                            common_music = []
-                    except KeyError:
-                        continue
-        except KeyError:
-            pass
+        return target, searched_users
 
+    def music_filter(self):
+        source = self.search_users_by_filter()
+        target = source[0]
+        searched_users = source[1]
+        music_filter = target['music']
+        common_music = list()
+        if ',' in music_filter:
+            music_filter = music_filter.split(',')
+        for user in searched_users:
+            if type(music_filter) == list:
+                try:
+                    for element in music_filter:
+                        a = re.search(re.compile(element, re.IGNORECASE), user['music'])
+                        if a is not None:
+                            common_music.append(a)
+                    if len(common_music) > 0:
+                        user['weight'] += 3
+                        common_music = []
+                except KeyError:
+                    continue
+            else:
+                try:
+                    a = re.search(re.compile(music_filter, re.IGNORECASE), user['music'])
+                    if a is not None:
+                        user['weight'] += 3
+                except KeyError:
+                    continue
+        return target, searched_users
+
+    def movies_filter(self):
+        source = self.music_filter()
+        target = source[0]
+        searched_users = source[1]
+        movies_filter = target['movies']
+        common_movies = list()
+        if ',' in movies_filter:
+            movies_filter = movies_filter.split(',')
+        for user in searched_users:
+            if type(movies_filter) == list:
+                try:
+                    for element in movies_filter:
+                        a = re.search(re.compile(element, re.IGNORECASE), user['movies'])
+                        if a is not None:
+                            common_movies.append(a)
+                    if len(common_movies) > 0:
+                        user['weight'] += 2
+                        common_movies = []
+                except KeyError:
+                    continue
+            else:
+                try:
+                    a = re.search(re.compile(movies_filter, re.IGNORECASE), user['movies'])
+                    if a is not None:
+                        user['weight'] += 2
+                except KeyError:
+                    continue
         return searched_users
+
 
     def mutual_friends_filter(self):
         target_uids_lst = []
-        searched_users = self.search_users_by_filter()
+        searched_users = self.movies_filter()
         offset = 0
         people_with_mutual = list()
         for user in searched_users:
             user_id = str(user["id"])
+            user['link'] = f'https://vk.com/id{user_id}'
             target_uids_lst.append(user_id)
         for index in range(100, len(target_uids_lst), 100):
             user_ids = ','.join(target_uids_lst[offset: index - 1])
             offset += 100
             response = requests.get(
-                "https://api.vk.com/method/friends.getMutual",
-                params={
-                    "access_token": self.access_token,
-                    "v": "5.103",
-                    "source_uid": self.resolveScreenName(self.id),
-                    "target_uids": user_ids,
-                }
-            )
+                f"{self.API}/friends.getMutual",
+                params={"access_token": self.access_token, "v": self.V, "source_uid": self.resolveScreenName(self.id),
+                    "target_uids": user_ids})
             time.sleep(0.3)
             print('***************')
             mutual_friends_info = response.json()["response"]
@@ -199,19 +219,9 @@ class User:
         return search_list
 
     def get_top_10(self):
-        most_common = list()
-        photos = list()
-        searced_list = self.get_groups_by_execute()
-        df = json_normalize(searced_list)
-        df = df.sort_values(by="weight", ascending=False).head(10)
-        df = df['id'].to_list()
-
-        for user in searced_list:
-            if user['id'] in df:
-                most_common.append(user)
-        for user in most_common:
-            user['link'] = 'https://vk.com/id' + str(user['id'])
-        return most_common
+        searched_list = self.get_groups_by_execute()
+        searched_list.sort(key=lambda searched_list: searched_list['weight'], reverse=True)
+        return searched_list[:10]
 
     def get_top3_photos(self):
         users = self.get_top_10()
@@ -222,6 +232,12 @@ class User:
             params = {'owner_id': user['id'], 'access_token': self.access_token,
                       'album_id': 'profile', 'extended': 1, 'photo_sizes': 1, 'v': self.V}
             user_id = user['id']
+            user.pop('can_access_closed')
+            user.pop('is_closed')
+            user.pop('track_code')
+            user.pop('weight')
+            if 'music' in user:
+                user.pop('music')
             try:
                 response = requests.get(f'{self.API}/photos.get', params=params).json()['response']
                 photos.append(response)
@@ -232,28 +248,22 @@ class User:
                             url = size['url']
                             a.append({'likes': likes, 'url': url})
                     user_photos[user_id] = a
+                    user['photo'] = user_photos[user['id']]
                 a = []
             except KeyError:
                 continue
         for element in user_photos:
             for_sort = user_photos[element]
-            for_sort.sort(key=lambda for_sort: for_sort['likes'], reverse= True)
+            for_sort.sort(key=lambda for_sort: for_sort['likes'], reverse=True)
             for index in for_sort:
-               a.append(index['url'])
+                a.append(index['url'])
             user_photos[element] = a[:3]
             a = []
         for user in users:
             if user['id'] in user_photos:
                 user['photo'] = user_photos[user['id']]
-            user.pop('can_access_closed')
-            user.pop('is_closed')
-            user.pop('track_code')
-            user.pop('weight')
-            try:
-                user.pop('music')
-            except KeyError:
-                continue
         return users
+
     def write_json(self):
         json_dict = self.get_top3_photos()
         with open('most_common_people.json', 'w', encoding='utf-8') as f:
@@ -274,9 +284,9 @@ class User:
         most_common_people_collection.insert_many(list_for_database)
         return list_for_database
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     user_id = input("Введите id в формате idXXXXX, или screen name пользователя: ")
     user = User(user_id)
+    access_token = user.authorization()
     pprint(user.write_to_db('most_common_people.json'))
-
--- tests/tests.py
+++ tests/tests.py
@@ -1,28 +1,33 @@
 import unittest
 from app import User
-from data import vk_api
 from unittest.mock import patch
 
 
 class TestMainApiFunctions(unittest.TestCase):
     def setUp(self):
-        self.user = User("ilya_dmitrenko")
-        self.api_key = vk_api
-        #self.user.authorization = '74467c5456ee9df843ce8f48ddb38c437fb301efd7e381fafde1081347130341b04c3a862d94464984dc2'
+        self.user = User("id72625041")
 
 
     def test_auth(self):
         with patch('app.input',
-                   return_value=''): # подставить токен, который генерируется после перехода по ссылке
+                   return_value='d5d6f9950576942f1a5fc9bc5bcfe068b7507eabb84b32541f43f73672d2e5ca647e75a9a6ba7fede7ccb'):
             result = self.user.authorization()
-            print(result)
         self.assertEqual(len(result), 85)
 
     def test_users_bdate(self):
-        with patch('app.input', return_value='RHCP'):
+        with patch('app.input',
+                   return_value='d5d6f9950576942f1a5fc9bc5bcfe068b7507eabb84b32541f43f73672d2e5ca647e75a9a6ba7fede7ccb'):
+            self.user.authorization()
+        with patch('app.input', side_effect=['RHCP', 'movies']):
             result = self.user.getUserInfo()
-
         self.assertGreater(len(result["bdate"]), 8)
+
+    def test_user_groups_count(self):
+        with patch('app.input',
+                   return_value='d5d6f9950576942f1a5fc9bc5bcfe068b7507eabb84b32541f43f73672d2e5ca647e75a9a6ba7fede7ccb'):
+            self.user.authorization()
+        result = self.user.get_groups()
+        self.assertEqual(len(result), 51)
 
     def test_find_duplicates_in_database(self):
         db_connect = self.user.connect_to_db()
@@ -33,8 +38,10 @@ class TestMainApiFunctions(unittest.TestCase):
         self.assertEqual(len(check_user_existance_lst), len(set(check_user_existance_lst)))
 
     def test_write_in_database(self):
-        with patch('app.input', side_effect='a'):
-
+        with patch('app.input',
+                   return_value='d5d6f9950576942f1a5fc9bc5bcfe068b7507eabb84b32541f43f73672d2e5ca647e75a9a6ba7fede7ccb'):
+            self.user.authorization()
+        with patch('app.input', side_effect=['RHCP', 'movies', '1993']):
             db_connect = self.user.connect_to_db()
             check_user_existance = list(db_connect.find())
             self.user.write_to_db('most_common_people.json')
